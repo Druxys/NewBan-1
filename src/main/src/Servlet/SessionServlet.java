8b85bfdc485df19e1983510055e38230fdddfaba
Alexandre <pro.conte.alexandre@gmail.com>
2019-11-02 17:54:29 +0100

    mise en route des sessions par les cookies

--- src/main/src/Servlet/SessionServlet.java
+++ src/main/src/Servlet/SessionServlet.java
@@ -2,6 +2,7 @@ package Servlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,9 +12,28 @@ import java.io.IOException;
 public class SessionServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String nom = request.getParameter("nom");
+        String prenom = request.getParameter("prenom");
+
+        Cookie cookie = new Cookie("prenom", prenom);
+        cookie.setMaxAge(60);
+        response.addCookie(cookie);
+
+        request.getRequestDispatcher("session.jsp").forward(request, response);
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("prenom")) {
+                    request.setAttribute("prenom", cookie.getValue());
+                }
+            }
+        }
+        request.getRequestDispatcher("session.jsp").forward(request, response);
 
     }
 }
--- src/main/webapp/session.jsp
+++ src/main/webapp/session.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%--
   Created by IntelliJ IDEA.
   User: shadownluffy
@@ -7,5 +8,20 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@include file="structures/header.jsp"%>
+
+<%String prenom = (String) request.getAttribute("prenom");%>
+<%=prenom%>
+<form method="post" action="session">
+    <p>
+        <label for="nom">Nom : </label>
+        <input type="text" name="nom" id="nom" />
+    </p>
+    <p>
+        <label for="prenom">Prénom : </label>
+        <input type="text" name="prenom" id="prenom" />
+    </p>
+
+    <input type="submit" />
+</form>
 
 <%@include file="structures/footer.jsp"%>
