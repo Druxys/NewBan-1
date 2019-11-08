bebaa0ebeba26284659b28919d283bcf59a28951
ConteAlexandre <theluffy@hotmail.fr>
2019-11-08 11:15:02 +0100

    model Historique

--- src/main/src/Utils/Database.java
+++ src/main/src/Utils/Database.java
@@ -22,7 +22,7 @@ public class Database {
             try
             {
                 Class.forName("org.postgresql.Driver");
-                db = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test", "postgres", "postgres");
+                db = DriverManager.getConnection("jdbc:postgresql://localhost:5432/newban", "postgres", "postgres");
             }
             catch(Exception e)
             {
